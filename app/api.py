1037ee031f0e5a2888d29922b8699d4472344ccb
Your Name <you@example.com>
2024-03-19 18:31:30 +0400

    refactor

--- app/api.py
+++ app/api.py
@@ -1,23 +1,33 @@
 import uuid
 from fastapi import FastAPI
-from app.main import dogs_facts_api
-from app.schemas import AddFacts
+from app.db import dogs_facts_api
+from app import schemas
 from datetime import datetime
 
 app = FastAPI()
 
 
-@app.post("/api/v1/facts/", status_code=201)
-def post_fact(fact: AddFacts) -> dict:
+@app.post(
+    "/api/v1/facts/",
+    response_model=schemas.FactsResponse,
+    status_code=201,
+)
+def post_fact(fact: schemas.PostFactsRequest):
     fact_id = str(uuid.uuid4())
     created_at = str(datetime.now())
-    response = {'fact_id': fact_id,
-                'fact_text': fact.fact_text,
-                'interest': fact.interest,
-                'likes': fact.likes,
-                'created_at': created_at}
-    dogs_facts_api.add_fact(response)
-    return response
+    fact = {'fact_id': fact_id,
+            'fact_text': fact.fact_text,
+            'interest': fact.interest,
+            'likes': fact.likes,
+            'created_at': created_at}
+    dogs_facts_api.add_fact(fact)
+    return schemas.FactsResponse(
+        id=fact["fact_id"],
+        fact_text=fact["fact_text"],
+        interest=fact["interest"],
+        likes=fact["likes"],
+        created_at=fact["created_at"],
+    )
 
 
 @app.get("/api/v1/facts", status_code=200)
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,13 @@
+import uuid
+
 from pydantic import BaseModel
 
 
-class AddFacts(BaseModel):
+class PostFactsRequest(BaseModel):
     fact_text: str
     interest: int
     likes: int = 0
+
+
+class FactsResponse(PostFactsRequest):
+    id: uuid.UUID
